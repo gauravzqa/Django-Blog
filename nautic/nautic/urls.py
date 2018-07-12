63ef2c11d48acf68dc5a6e7ae5ba31471ed90558
gauravzqa <gauravzqa@gmail.com>
2018-07-12 14:16:36 +0530

    media upload enabled

--- nautic/nautic/urls.py
+++ nautic/nautic/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^articles/', include('articles.urls')),
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
@@ -12,3 +15,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
